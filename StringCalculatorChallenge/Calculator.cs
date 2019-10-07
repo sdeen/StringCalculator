9af27f71b9b97aecd98fe9f1406ae6a01b5d6005
Sultana Deen <pr33tysimac@Sultanas-iMac-3.local>
2019-10-07 19:38:10 -0400

    Step#1

---
+++ StringCalculatorChallenge/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorChallenge
+{
+    public static class Calculator
+    {
+        public static int AddNumber(string number)
+        {
+            int result = 0;
+            try
+            {
+                Regex pattern = new Regex(@"([a-zA-Z])");
+                var arrayNumber = number.Split(',');
+                //Step #1
+                if (string.IsNullOrEmpty(number))
+                {
+                    result = 0;
+                }
+                else if (number.Contains(",,"))
+                {
+                    var getNumber = number.Where(x => char.IsDigit(x)).ToArray();
+                    result = getNumber.Sum(n => int.Parse(n.ToString()));
+                }
+                else if (number.Contains(",") && !pattern.IsMatch(number))
+                {
+                    result = arrayNumber.Sum(n => int.Parse(n));
+                }
+                else if (pattern.IsMatch(number))
+                {
+                    var getNumber = number.Where(x => char.IsDigit(x)).ToArray();
+                    result = getNumber.Sum(n => int.Parse(n.ToString()));
+                }
+                else if (arrayNumber.Any(n => int.Parse(n) < 0))
+                {
+                    return 1;
+                }
+                else
+                {
+                    result = int.Parse(number);
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ StringCalculatorChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter maximum 2 numbers using a comma delimiter to add the numbers. For Example: 1,1 = 1+1=2:");
+                string number = "";
+                number = Convert.ToString(Console.ReadLine());
+                int length = String.IsNullOrEmpty(number) ? 0 : number.Length;
+                int count = number.Count(c => c == ',');
+                if (count > 1)
+                {
+                    Console.WriteLine("Error:Input must be maximum 2 numbers using a comma delimiter");
+                }
+                else
+                {
+                    int result = Calculator.AddNumber(number);
+                    Console.WriteLine($"Result: {result}");
+                }
+
+
+                Console.WriteLine("Enter quite to close the Calculator console. OR Enter to continue");
+                string input = Console.ReadLine();
+                if (input == "quite")
+                {
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
